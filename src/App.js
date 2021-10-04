f04cd6f3998cc6cd5217e3d19c004f9252b536ce
Hafiz <hasnathafiz229>
2021-10-04 12:38:13 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <p className="bg-yellow-200 text-4xl hover:text-blue-900">
-        This is my text
+        React Tailwind CSS
       </p>
-      <p className="text-primary">Hello my text</p>
+      <p className="text-primary">Tailwind in React</p>
       <p className="text-myCustom">Hello my text</p>
     </div>
   );
